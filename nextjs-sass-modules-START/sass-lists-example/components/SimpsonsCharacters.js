f0b580109f1928a28800fbe6cc807b8e7f0f4848
iilmi2 <iilmi2@nait.ca>
2023-01-30 09:51:35 -0700

    SASS Example

---
+++ nextjs-sass-modules-START/sass-lists-example/components/SimpsonsCharacters.js
@@ -0,0 +1,20 @@
+import styles from '../styles/SimpsonsChar.module.scss'
+const SIMPSON_CHARACTERS = [
+  "Homer Simpson",
+  "Bart Simpson",
+  "Marge Simpson",
+  "Mr. Burns",
+  "Lisa Simpson",
+  "Apu Nahasapeemapetilon",
+  "Sideshow Bob",
+  "Milhouse Van Houten",
+  "Ned Flanders",
+]
+
+export default function SimpsonsCharacters() {
+  return <ul className={styles.list}>
+    {SIMPSON_CHARACTERS.map((characterName, index)=> {
+      return <li key={index} className= {`${styles['list-item']} ${styles.spacing}`}>{characterName}</li>
+    })}
+  </ul>
+}
